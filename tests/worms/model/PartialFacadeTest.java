3d7112d5f0eb583d16b7d6eefa4439e75fc75e97
Brancus <brent.coosemans@hotmail.com>
2014-04-29 10:27:10 +0200

    * *

--- tests/worms/model/PartialFacadeTest.java
+++ tests/worms/model/PartialFacadeTest.java
@@ -1,9 +1,6 @@
 package worms.model;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.Random;
 
